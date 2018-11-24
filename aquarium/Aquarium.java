be52a465ba62eb64da36d112f6a8a935477143ea
Ryan <ryandmrr@gmail.com>
2018-11-24 22:07:59 +0100

    Add a constructor to the Aquarium for the Color

--- aquarium/Aquarium.java
+++ aquarium/Aquarium.java
@@ -1,8 +1,10 @@
 package aquarium;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
 
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 
 import aquarium.items.AquariumItem;
@@ -12,17 +14,15 @@ public class Aquarium extends JPanel
 	static int NB_STONES, NB_SEAWEED;
 	
     private ArrayList<AquariumItem> items = new ArrayList<AquariumItem>();
-
-    Aquarium()
-    {
-
-    }
-
+    
     public void fill(AquariumItem item)
     {
         this.items.add(item);
     }
-
+    public Aquarium()
+    {
+    	this.setBackground(Color.BLUE);
+    }
     @Override
     public void paint(Graphics g)
     {
