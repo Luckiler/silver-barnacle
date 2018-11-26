ab7e7a54d56c7bb8b8f774dd6198a27020fee9c1
Lucas Viegas Milani <luckiler3@gmail.com>
2018-11-26 14:00:06 +0100

    Added MobileItem stub

---
+++ aquarium/items/MobileItem.java
@@ -0,0 +1,15 @@
+package aquarium.items;
+
+import java.awt.Point;
+
+import aquarium.Mobile;
+
+public abstract class MobileItem extends AquariumItem implements Mobile
+{
+	static final int BASE_SPEED = 10;
+	
+	public boolean move(Point destination)
+	{
+		
+	}
+}
