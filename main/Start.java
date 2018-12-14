afffa16296f967dfefaad8d2a6668e820e9e10bf
Lucas Viegas Milani <luckiler3@gmail.com>
2018-12-14 16:47:20 +0100

    Lambda expression

--- main/Start.java
+++ main/Start.java
@@ -6,10 +6,6 @@ import animation.Animation;
 public class Start
 {
     public static void main(String[] args) {
-    	SwingUtilities.invokeLater(new Runnable(){
-			public void run() {
-				new Animation("Aquarium");
-			}
-    	});
+    	SwingUtilities.invokeLater(() -> new Animation("Aquarium"));
     }
 }
