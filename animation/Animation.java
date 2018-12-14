8e2f04826e0f3aa945ce0684afca8f155f554a70
Ryan <ryandmrr@gmail.com>
2018-12-14 14:57:16 +0100

    Add a new background called "Fond océan" to the Aquarium

--- animation/Animation.java
+++ animation/Animation.java
@@ -1,6 +1,8 @@
 package animation;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -19,10 +21,12 @@ public class Animation extends JFrame {
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	setResizable(false);
 	setLocationRelativeTo(null);
-	getContentPane().setBackground(Color.BLUE);
-	
 	JPanel Aquarium =new Aquarium();
 	add(Aquarium);
+	setLayout(new BorderLayout());
+    JLabel background=new JLabel(new ImageIcon("fond océan.jpg"));
+    add(background);
+    background.setLayout(new FlowLayout());
 	
     this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
     this.addWindowListener(new WindowAdapter()
