ad75dd511c1a123ceeb83330bd1563858707e978
Ryan <ryandmrr@gmail.com>
2018-12-14 13:40:09 +0100

    Add the fish --> fish now is a fish

--- aquarium/items/Fish.java
+++ aquarium/items/Fish.java
@@ -1,14 +1,27 @@
 package aquarium.items;
 
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Fish extends MobileItem {
-
 	@Override
 	public void draw(Graphics g) {
-		g.setColor(Color.red);
-		g.fillOval(this.position.x, this.position.y, this.width, this.height);
+		BufferedImage image;
+		try {
+			image = ImageIO.read(new File("Poisson.png"));
+			g.drawImage(image, position.x, position.y, width, height, null);
+		} catch (IOException e) {
+			e.printStackTrace();
+	}	
+}
+	public Fish(Point point, int width, int height){
+		this.position = point;
+		this.width = width;
+		this.height = height;
 	}
-
 }
